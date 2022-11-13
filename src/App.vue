6742c197e798535589039dd3c2382f2b8dc7909a
chris chen <christinachen149@gmail.com>
2022-11-13 23:13:42 +0800

    deploy

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div class="group border-indigo-500 hover:bg-white hover:shadow-lg hover:border-transparent ...">
+  <p class="text-indigo-600 group-hover:text-gray-900 ...">New Project</p>
+  <p class="text-indigo-500 group-hover:text-gray-500 ...">Create a new project from a variety of starting templates.</p>
+</div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
